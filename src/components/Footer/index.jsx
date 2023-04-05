4f569af7762aca3fe4d232b09860af3e0dae548f
Kaio Bruno Barbosa Magalhães <bmkaiobruno@gmail.com>
2023-04-05 11:56:39 -0300

    Test 2

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,18 +3,14 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="flex justify-center items-start gap-5 mt-2 lg:justify-start lg:mx-[17.5rem]">
-      <img
-        className="h-4"
-        src="/public/images/client-databiz.svg"
-        alt="Databiz"
-      />
+      <img className="h-4" src="/public/client-databiz.svg" alt="Databiz" />
       <img
         className="h-6"
-        src="/public/images/client-audiophile.svg"
+        src="/public/client-audiophile.svg"
         alt="Audio phile"
       />
-      <img className="h-4" src="/public/images/client-meet.svg" alt="Meet" />
-      <img className="h-5" src="/public/images/client-maker.svg" alt="Maker" />
+      <img className="h-4" src="/public/client-meet.svg" alt="Meet" />
+      <img className="h-5" src="/public/client-maker.svg" alt="Maker" />
     </footer>
   );
 };
